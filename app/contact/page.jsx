9d826507ccc9a867498c629a36bdbd4f6c8be542
Viktor Karpinski <developer@viktorkarpinski.com>
2025-06-12 22:57:24 +0200

    Current

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -4,7 +4,12 @@ const Contact = () => {
     return (
         <>
             <section id="contact">
+                <div className="wrapper">
+                     <h1>
+                        Contact
+                    </h1>
 
+                </div>
             </section>
         </>
     )
