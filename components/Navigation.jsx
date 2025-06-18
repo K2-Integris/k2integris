148f99c1ac9f804abcfaa0b4adf211fb6d9bff1f
Viktor Karpinski <developer@viktorkarpinski.com>
2025-06-18 18:49:23 +0200

    Navigation update and burger animation

--- components/Navigation.jsx
+++ components/Navigation.jsx
@@ -1,20 +1,28 @@
+"use client"
+
 import Image from "next/image";
 import Link from "next/link";
+import { useRef } from "react";
 
 const Navigation = () => {
+
+    const menu = useRef()
+
+    const handleMenu = () => {
+        menu.current.classList.toggle('open')
+    }
+
     return (
         <nav>
             <div className="wrapper">
-                <div className="logo-wrapper">
-                    <Link id="logo" href="https://k2integris.com">
-                        <Image src="/images/logo.png" width={29} height={17} alt="K2Integris Logo" />
-                    </Link>
-                    <Link id="text-logo" href="https://k2integris.com">
-                        K2INTEGRIS
-                    </Link>
-                </div>
+                <Link id="logo" href="https://k2integris.com">
+                    <Image src="/images/logo.png" width={29} height={17} alt="K2Integris Logo" />
+                </Link>
+                <Link id="text-logo" href="https://k2integris.com">
+                    K2INTEGRIS
+                </Link>
 
-                <button id="hamburger">
+                <button id="hamburger" onClick={handleMenu} ref={menu}>
                     <span></span>
                     <span></span>
                     <span></span>
