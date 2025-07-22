67a9facc7eabddd1237bfcf5140f263e5021e31f
Viktor Karpinski <developer@viktorkarpinski.com>
2025-07-22 11:00:47 +0200

    Imprint v1

--- app/imprint/page.jsx
+++ app/imprint/page.jsx
@@ -1,8 +1,36 @@
-const { default: ComingSoon } = require("../coming-soon")
+import ContactCTA from '@/components/ContactCTA'
+import styles from './styles.module.css'
 
 const Imprint = () => {
     return (
-        <ComingSoon />
+        <>
+            <section className={styles.imprint}>
+                <div className="wrapper">
+                    <article>
+                        <h1>
+                            Imprint
+                        </h1>
+                        <h3>
+                            K2Integris Sp. z o.o.
+                        </h3>
+
+                        <p>
+                            Przyszowa 146<br />34-604 Przyszowa<br />POLAND
+                        </p>
+
+                        <p>
+                            NIP: <span>872-21-52-522</span>
+                        </p>
+
+                        <p>
+                            REGON: <span>691691523</span>
+                        </p>
+                    </article>
+
+                    <aside></aside>
+                </div>
+            </section>
+        </>
     )
 }
 
