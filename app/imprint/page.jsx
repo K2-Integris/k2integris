206845055d51e18af6f377ce409f9796a731002e
Viktor Karpinski <developer@viktorkarpinski.com>
2025-07-22 11:34:12 +0200

    Imprint v2

--- app/imprint/page.jsx
+++ app/imprint/page.jsx
@@ -1,4 +1,3 @@
-import ContactCTA from '@/components/ContactCTA'
 import styles from './styles.module.css'
 
 const Imprint = () => {
@@ -6,28 +5,84 @@ const Imprint = () => {
         <>
             <section className={styles.imprint}>
                 <div className="wrapper">
-                    <article>
+                    <article className={styles.container}>
                         <h1>
                             Imprint
                         </h1>
-                        <h3>
-                            K2Integris Sp. z o.o.
-                        </h3>
+                        <div className={styles.block}>
+                            <h3>
+                                K2Integris Sp. z o.o.
+                            </h3>
 
-                        <p>
-                            Przyszowa 146<br />34-604 Przyszowa<br />POLAND
-                        </p>
+                            <p>
+                                <span>
+                                    Registered Office:
+                                </span>
+                                <br />
+                                Przyszowa 146, 34-604 Przyszowa, POLAND
+                            </p>
 
-                        <p>
-                            NIP: <span>872-21-52-522</span>
-                        </p>
+                            <p>
+                                <span>
+                                    Court Of Registration:
+                                </span>
+                                <br />
+                                Przyszowa 146, 34-604 Przyszowa, POLAND
+                            </p>
 
-                        <p>
-                            REGON: <span>691691523</span>
-                        </p>
+                            <p>
+                                <span>
+                                    KRS Number:
+                                </span>
+                                <br />
+                                0000123456
+                            </p>
+
+                            <p>
+                                <span>
+                                    Tax Identification Number (NIP):
+                                </span>
+                                <br />
+                                123-456-78-90
+                            </p>
+
+                            <p>
+                                <span>
+                                    Share Capital:
+                                </span>
+                                <br />
+                                PLN 5.000,00 (fully paid)
+                            </p>
+                        </div>
+
+                        <div className={styles.block}>
+                            <h3>
+                                Viktor Karpinski
+                            </h3>
+
+                            <p>
+                                +48 731 499 788
+                            </p>
+
+                            <p>
+                                karpinski<span className={styles.at}>@</span>k2integris.com
+                            </p>
+                        </div>
+
+                        <div className={styles.block}>
+                            <h3>
+                                Mateusz Kaminski
+                            </h3>
+
+                            <p>
+                                +48 731 499 788
+                            </p>
+
+                            <p>
+                                kaminski<span className={styles.at}>@</span>k2integris.com
+                            </p>
+                        </div>
                     </article>
-
-                    <aside></aside>
                 </div>
             </section>
         </>
