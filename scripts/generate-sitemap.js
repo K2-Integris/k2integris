859c63b524416facc94eb53569dc5f43c20b360c
Viktor Karpinski <developer@viktorkarpinski.com>
2025-07-28 11:59:50 +0200

    v2

--- scripts/generate-sitemap.js
+++ scripts/generate-sitemap.js
@@ -1,45 +1,47 @@
 const { SitemapStream, streamToPromise } = require("sitemap");
-const { createWriteStream } = require("fs");
 const { resolve } = require("path");
+const { writeFileSync } = require("fs");
+const { Readable } = require("stream");
 
 (async () => {
-  const sitemap = new SitemapStream({ hostname: "https://k2integris.com" });
-
-  const baseUrls = [
-    { lang: "en", domain: "https://k2integris.com" },
-    { lang: "pl", domain: "https://k2integris.pl" },
+  const routes = [
+    "/",
+    "/about",
+    "/services",
+    "/web-development",
+    "/app-development",
+    "/e-commerce",
+    "/marketing-and-seo",
+    "/work",
+    "/contact",
+    "/privacy-policy",
+    "/imprint",
+    "/terms-of-use",
+    "/testimonials",
   ];
 
-  const pages = [
-    { path: "/", changefreq: "weekly", priority: 1.0 },
-    { path: "/about", changefreq: "monthly" },
-    { path: "/services", changefreq: "monthly" },
-    { path: "/web-development", changefreq: "monthly" },
-    { path: "/app-development", changefreq: "monthly" },
-    { path: "/e-commerce", changefreq: "monthly" },
-    { path: "/marketing-and-seo", changefreq: "monthly" },
-    { path: "/work", changefreq: "monthly" },
-    { path: "/contact", changefreq: "yearly" },
-    { path: "/privacy-policy", changefreq: "yearly" },
-    { path: "/imprint", changefreq: "yearly" },
-    { path: "/terms-of-use", changefreq: "yearly" },
-    { path: "/testimonials", changefreq: "monthly" },
-  ];
+  const stream = new SitemapStream({
+    hostname: "https://k2integris.com",
+    xmlns: {
+      xhtml: true,
+    },
+  });
 
-  pages.forEach(({ path, changefreq, priority }) => {
-    sitemap.write({
+  routes.forEach((path) => {
+    stream.write({
       url: path,
-      changefreq,
-      priority,
-      links: baseUrls.map(({ lang, domain }) => ({
-        lang,
-        url: domain + path,
-      })),
+      changefreq:
+        path === "/" ? "weekly" : path === "/contact" ? "yearly" : "monthly",
+      priority: path === "/" ? 1.0 : undefined,
+      links: [
+        { lang: "en", url: `https://k2integris.com${path}` },
+        { lang: "pl", url: `https://k2integris.pl${path}` },
+      ],
     });
   });
 
-  sitemap.end();
+  stream.end();
 
-  const xml = await streamToPromise(sitemap);
-  createWriteStream(resolve(__dirname, "../public/sitemap.xml")).end(xml);
+  const xml = await streamToPromise(stream);
+  writeFileSync(resolve(__dirname, "../public/sitemap.xml"), xml.toString());
 })();
