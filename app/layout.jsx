b28ecdf7aab976fc71a8b6be4966b3a66bb1739b
viktor <viktor@viktors-MacBook-Pro.local>
2025-08-26 11:56:58 +0200

    server

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,4 +1,4 @@
-import "./css/style.css";
+//import "./css/style.css";
 import Navigation from "@/components/Navigation";
 import Faq from "@/components/Faq";
 import Footer from "@/components/Footer";
@@ -11,10 +11,10 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-       {/*
+       
         <link rel="preload" href="/css/base.css" as="style" />
         <link rel="stylesheet" href="/css/base.css" />
-        */}
+        
         <meta name="apple-mobile-web-app-title" content="K2Integris" />
         <link rel="icon" href="/favicon.ico" type="image/x-icon" />
         <meta name="application-name" content="K2Integris" />
